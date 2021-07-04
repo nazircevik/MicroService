8a8084e85f7ee0d49b37dc77ab7709c17c522843
Nazir Çevik <nzr.cevik@hotmail.com>
2021-07-04 21:46:31 +0300

    appsettings

--- MicroService/Services/Catalog/FreeCourse.Services.Catolog/Startup.cs
+++ MicroService/Services/Catalog/FreeCourse.Services.Catolog/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FreeCourse.Services.Catolog.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace FreeCourse.Services.Catolog
 {
@@ -26,6 +28,13 @@ namespace FreeCourse.Services.Catolog
         {
             services.AddControllers();
             services.AddAutoMapper(typeof(Startup));
+            services.Configure<DatabaseSettings>(Configuration.GetSection("DatabaseSettings"));
+            services.AddSingleton<IDatabaseSettings>(
+                sp => {
+                    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+                
+                
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
