3072c70e36f96980b472ccc1352d3db2ce9f3e3a
Nazir Çevik <nzr.cevik@hotmail.com>
2021-07-04 16:17:54 +0300

    mapping

---
+++ MicroService/Services/Catalog/FreeCourse.Services.Catolog/Mapping/GeneralMapping.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Catolog.Dtos;
+using FreeCourse.Services.Catolog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catolog.Mapping
+{
+    public class GeneralMapping:Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Course, CourseDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Feature, FeatureDto>().ReverseMap();
+
+            CreateMap<Course, CourseCrateDto>().ReverseMap();
+            CreateMap<Course, CourseUpdateDto>().ReverseMap();
+        }
+    }
+}
